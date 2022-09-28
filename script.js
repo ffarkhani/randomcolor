1b85dc98ef2fa72910bbb0a6e6c618981d676ff7
Farshad Farkhani <farshad.farkhani69@gmail.com>
2022-09-28 09:29:20 +0330

    Optimize project

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
-const redRange = document.getElementById("red-range");
-const greenRange = document.getElementById("green-range");
-const blueRange = document.getElementById("blue-range");
-const btnChange = document.getElementById("btn-change");
+const btnGenerate = document.getElementById("btn-change");
 const hexCode = document.getElementById("hex-code");
 const copyMessage = document.getElementById("copy-message");
 const hexCopyContainer = document.querySelector("div.hexcopy");
+const rangesContainer = document.getElementById("ranges-container");
 const body = document.body;
 
-changeBodyBackground(redRange.value, greenRange.value, blueRange.value);
+let colors = {
+    red: 255,
+    green: 255,
+    blue: 255,
+};
+
+generateColorsAndApplyToRanges();
 copyMessage.textContent = "";
 
-hexCopyContainer.addEventListener("click", (e) => {
+rangesContainer.addEventListener("input", function (e) {
+    colors[e.target.name] = e.target.value;
+    changeBackgroundAndColorCode();
+});
+
+btnGenerate.addEventListener("click", () => {
+    generateColorsAndApplyToRanges();
+});
+
+hexCopyContainer.addEventListener("click", () => {
     navigator.clipboard.writeText(hexCode.textContent);
     copyMessage.textContent = "Copied";
+    copyMessage.style.color = `rgb(${colors.red}, ${colors.green}, ${colors.blue})`;
     setTimeout(() => {
         copyMessage.textContent = "";
     }, 1500);
 });
 
-redRange.addEventListener("input", (e) => {
-    changeBodyBackground(e.target.value, greenRange.value, blueRange.value);
-});
-greenRange.addEventListener("input", (e) => {
-    changeBodyBackground(redRange.value, e.target.value, blueRange.value);
-});
-blueRange.addEventListener("input", (e) => {
-    changeBodyBackground(redRange.value, greenRange.value, e.target.value);
-});
-btnChange.addEventListener("click", () => {
-    const redColor = generateRandomColor();
-    const greenColor = generateRandomColor();
-    const blueColor = generateRandomColor();
-    redRange.value = redColor;
-    greenRange.value = greenColor;
-    blueRange.value = blueColor;
-
-    changeBodyBackground(redColor, greenColor, blueColor);
-});
-
-function changeColor(r = 0, g = 0, b = 0) {
-    return {
-        red: r,
-        green: g,
-        blue: b,
-    };
+function changeBackgroundAndColorCode() {
+    body.style.backgroundColor = `rgb(${colors.red}, ${colors.green}, ${colors.blue})`;
+    rgb2hex();
 }
 
-function generateRandomColor() {
+function generateColorsAndApplyToRanges() {
+    colors = {
+        red: generateRandomNumber(),
+        green: generateRandomNumber(),
+        blue: generateRandomNumber(),
+    };
+    for (const child of rangesContainer.children) {
+        child.value = colors[child.name];
+    }
+    changeBackgroundAndColorCode();
+}
+
+function generateRandomNumber() {
+    // between 0 to 255
     return Math.floor(Math.random() * (255 - 0 + 1)) + 0;
 }
 
-function changeBodyBackground(r = 0, g = 0, b = 0) {
-    const color = changeColor(r, g, b);
-    body.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
-    rgb2hex(color.red, color.green, color.blue);
-}
-
-function rgb2hex(r, g, b) {
+function rgb2hex() {
     hexCode.textContent =
-        "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+        "#" +
+        componentToHex(colors.red) +
+        componentToHex(colors.green) +
+        componentToHex(colors.blue);
 }
 
 function componentToHex(c) {
